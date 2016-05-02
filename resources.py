0f49bdaa3bb5bb88b76f4e2541189653cdae983b
JakeThurman <jacob@thurmans.com>
2016-05-01 23:26:21 -0400

    Delete some unneded resource strings

--- resources.py
+++ resources.py
@@ -3,11 +3,3 @@ YOU_WON = "Congratulations, You won!"
 YOU_LOST = "Better luck next time..."
 RETURN_TO_LEVEL_PICKER = "Choose A Level"
 PLAY_AGAIN = "Play Again"
-
-# Package Importer
-DOWNLOAD_PACKAGE_TO = "Download the package folder to:"
-ADD_PACKAGE_TEXT = "Then, add the name of the package folder here..."
-ADD_PACKAGE_PROMPT = "Package Folder Name"
-ADD_PACKAGE_FROM_FOLDER_NAME_BUTTON = "Import"
-BACK_TO_LEVEL_PICKER = "Back"
-IMPORT_PACKAGE = "Import Package"
